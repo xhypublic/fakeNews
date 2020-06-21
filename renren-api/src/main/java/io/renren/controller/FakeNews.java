fd25b33fe58dd72b6f4bd1ac43462c9a4b8b0205
wwwtaermelon <742361078@qq.com>
2020-06-21 18:23:02 +0800

    update

--- renren-api/src/main/java/io/renren/controller/FakeNews.java
+++ renren-api/src/main/java/io/renren/controller/FakeNews.java
@@ -9,15 +9,13 @@ import io.renren.entity.NewsEntity;
 import io.renren.utils.FileUtils;
 import io.renren.utils.IDUtils;
 import io.swagger.annotations.ApiOperation;
-import oracle.sql.TIMESTAMP;
-import org.springframework.http.HttpRequest;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServlet;
+import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Date;
+
 
 @RestController
 @RequestMapping("/api")
@@ -45,4 +43,27 @@ public class FakeNews {
         return R.ok().put("msg", "请求成功").put("result",newsEntity).put("code",200);
     }
 
+    @PostMapping("newsFile.json")
+    @ApiOperation("虚假新闻上传接口")
+    public R fakeNewsFiles(@RequestParam("file") MultipartFile file) throws IOException {
+        if (file.isEmpty()) {
+            return R.error().put("msg","参数传递错误");
+        }
+        byte[] b = file.getBytes();
+        String news = new String(b);
+        if(StringUtils.isEmpty(news)){
+            return R.error().put("msg","参数传递错误");
+        }
+        Date date = new Date();
+        String timeStamp = String.valueOf(date);
+        Long id = IDUtils.createID();
+        NewsEntity newsEntity = new NewsEntity();
+        newsEntity.setText(news);
+        newsEntity.setId(id);
+        newsEntity.setCreatedAt(timeStamp);
+        newsEntity.setInReplyToStatusId(id+"");
+        String json = objectMapper.writeValueAsString(newsEntity);
+        FileUtils.save2File("D:\\test\\unknow_label\\"+id+"\\source-tweet\\"+id+".json", json);
+        return R.ok().put("msg", "请求成功").put("result",newsEntity).put("code",200);
+    }
 }
